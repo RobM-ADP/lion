c87ce38302b72391c27932b3e508b2f4836168f1
erik <erik.kroes@ing.com>
2019-12-11 09:39:43 +0100

    chore: update docs and demos

--- packages/input-range/README.md
+++ packages/input-range/README.md
@@ -2,13 +2,12 @@
 
 [//]: # 'AUTO INSERT HEADER PREPUBLISH'
 
-`ing-input-range` component is based on the generic text input field.
+`lion-input-range` component is based on the native range input.
 Its purpose is to provide a way for users to select one value from a range of values.
 
 ## Features
 
 - based on [lion-input](../input)
-- makes use of range [validators](https://gitlab.ing.net/TheGuideComponents/ing-web/packages/validate/docs/DefaultValidators.md) with corresponding error messages in different languages
 
 ## How to use
 
@@ -19,11 +18,11 @@ npm i --save @lion/input-range
 ```
 
 ```js
-import '@lion/input-range/ing-input-range.js';
+import '@lion/input-range/lion-input-range.js';
 ```
 
 ### Example
 
 ```html
-<lion-input-range name="mySelectedRange"></lion-input-range>
+<lion-input-range min="200" max="500" modelValue="300" label="Input range"></lion-input-range>
 ```
--- packages/input-range/stories/index.stories.js
+++ packages/input-range/stories/index.stories.js
@@ -1,11 +1,114 @@
 import { storiesOf, html } from '@open-wc/demoing-storybook';
+import { css } from '@lion/core';
 
 import '../lion-input-range.js';
 
-storiesOf('Forms | Input Range', module).add(
-  'Default',
-  () => html`
-    <lion-input-range min="200" max="500" modelValue="300"></lion-input-range>
-    <lion-input-range min="50" max="200" step="10" value="150"></lion-input-range>
-  `,
-);
+const rangeDemoStyle = css`
+  .demo-range {
+    max-width: 400px;
+  }
+`;
+
+storiesOf('Forms | Input Range', module)
+  .add(
+    'Default',
+    () => html`
+      <style>
+        ${rangeDemoStyle}
+      </style>
+
+      <lion-input-range
+        class="demo-range"
+        min="200"
+        max="500"
+        modelValue="300"
+        label="Input range"
+      ></lion-input-range>
+    `,
+  )
+  .add(
+    'Help text',
+    () => html`
+      <style>
+        ${rangeDemoStyle}
+      </style>
+
+      <lion-input-range
+        class="demo-range"
+        min="200"
+        max="500"
+        modelValue="300"
+        label="Input range"
+        help-text="A help text can show additional hints"
+      ></lion-input-range>
+    `,
+  )
+  .add(
+    'Units',
+    () => html`
+      <style>
+        ${rangeDemoStyle}
+      </style>
+
+      <lion-input-range
+        class="demo-range"
+        min="0"
+        max="100"
+        modelValue="50"
+        unit="%"
+        label="Input range"
+      ></lion-input-range>
+    `,
+  )
+  .add(
+    'Help text',
+    () => html`
+      <style>
+        ${rangeDemoStyle}
+      </style>
+
+      <lion-input-range
+        class="demo-range"
+        min="200"
+        max="500"
+        modelValue="300"
+        label="Input range"
+        help-text="A help text can show additional hints"
+      ></lion-input-range>
+    `,
+  )
+  .add(
+    'With steps',
+    () => html`
+      <style>
+        ${rangeDemoStyle}
+      </style>
+
+      <lion-input-range
+        class="demo-range"
+        min="200"
+        max="500"
+        step="50"
+        modelValue="300"
+        label="Input range"
+        help-text="This slider uses increments of 50"
+      ></lion-input-range>
+    `,
+  )
+  .add(
+    'Disabled',
+    () => html`
+      <style>
+        ${rangeDemoStyle}
+      </style>
+
+      <lion-input-range
+        class="demo-range"
+        min="200"
+        max="500"
+        modelValue="300"
+        disabled
+        label="Input range"
+      ></lion-input-range>
+    `,
+  );
